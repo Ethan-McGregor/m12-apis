128a50241a7f9f6377d19d4e3bc7b55001e0895c
Ethan-McGregor <ethanm4@uw.edu>
2017-04-26 15:09:21 -0700

    worked on 4

--- exercise-4/exercise.r
+++ exercise-4/exercise.r
@@ -1,34 +1,46 @@
 # Install and load the jsonlite package
 library(jsonlite)
 library(httr)
-
+library(stringr)
 # Make a variable base.url that has the same base url from the omdb documentation.
 # (Hint: visit https://www.omdbapi.com/ to find the base url)
 
+GET('http://www.omdbapi.com/?')
+
 
 # Make a variable called movie that has the name of your favorite movie
+movie <- "Frozen" 
 
 
 # Make a variable called "query.parameters" that holds a list of the parameters
 # to pass to the API. View the OMDb documentation to see which parameters
 # are available.
 
+query.parameters <- c('t=')
 
 # Make a variable called request that is a string of a request URL made up of the base URL
 # and the parameters string
 
+request <- 'http://www.omdbapi.com/?t=frozen'
 
 # Use fromJSON to retrieve JSON data from the omdb api using your request.
 # Store the result in a variable called movie.data
 
+movie.data <- fromJSON(request)
 
 # Make movie_data into a data frame using as.data.frame
 
+movie.data <- as.data.frame(movie.data)
 
 # Write a function called Director that accepts a data frame of movie info and returns
 # A vector of strings that states a movie and the director of said movie.
 
+Director <- function(movie){
+  movie.director <- str_c( )
+  return(movie.director)
+}
 
+Director(movie.data)
 # Call Director with your favorite movie, and assign it to the variable movie.director
 
 
